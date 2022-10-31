acbb654f944d756c57d55a8d7ab38eb8649c00ca
Emmanuel Ikpesu <emmanarutops2@gmail.com>
2022-10-31 23:19:42 +0100

    Feat:Building binomial distribution package

---
+++ Software_Engineering_Practices_1/Package_installation/Distribution/__inti__.py
@@ -0,0 +1,1 @@
+from generaldistribution import Distribution
---
+++ Software_Engineering_Practices_1/Package_installation/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribution',
+    version = 0.1,
+    description='Guassian distribution',
+    packages=['Distribution'],
+    zip_safe=False)
